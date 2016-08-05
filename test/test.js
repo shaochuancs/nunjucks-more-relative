963b1d63088c7a0f398c6dfa562de5552c692d46
Chuan Shao <shaochuancs@gmail.com>
2016-08-05 16:43:07 +0800

    add travis

--- test/test.js
+++ test/test.js
@@ -4,3 +4,26 @@
 
 "use strict";
 
+var assert = require('assert');
+var moreRelative = require('../');
+
+var nunjucksMock = {Loader:{prototype:{isRelative:null}}};
+
+describe('Test module behaviour', function() {
+  moreRelative(nunjucksMock);
+  it('should return false when filename start with /', function() {
+    assert.equal(false, nunjucksMock.Loader.prototype.isRelative('/testpath/subpath'));
+  });
+
+  it('should return true when filename start with ./', function() {
+    assert.equal(true, nunjucksMock.Loader.prototype.isRelative('./testpath/subpath'));
+  });
+
+  it('should return true when filename start with ../', function() {
+    assert.equal(true, nunjucksMock.Loader.prototype.isRelative('../testpath/subpath'));
+  });
+
+  it('should return true when filename start with plain word', function() {
+    assert.equal(true, nunjucksMock.Loader.prototype.isRelative('testpath/subpath'));
+  });
+});
